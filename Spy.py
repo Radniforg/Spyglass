179a5438585c6cb7a65cb30b3d70408ab19f3c99
Ivan Alexandrov <radniforg@yandex.ru>
2020-04-30 17:26:34 +0300

    PEP8-correct version

--- Spy.py
+++ Spy.py
@@ -4,6 +4,7 @@ import requests
 import json
 import time
 
+
 def token_confirmation(app_id):
     token = ''
     check_bool = False
@@ -16,7 +17,7 @@ def token_confirmation(app_id):
             }
         )
         try:
-            error_code_5 = token_check.json()['error']
+            token = token_check.json()['error']
             OAUTH_URL = 'https://oauth.vk.com/authorize'
             OAUTH_PARAMS = {
                 'client_id': app_id,
@@ -26,13 +27,15 @@ def token_confirmation(app_id):
                 'v': 5.52
             }
             print('?'.join((OAUTH_URL, urlencode(OAUTH_PARAMS))))
-            token = input('Пожалуйста, пройдите по ссылке и вставьте корректный TOKEN:\n')
+            token = input('Пожалуйста, пройдите по ссылке и'
+                          ' вставьте корректный TOKEN:\n')
         except KeyError:
             check_bool = True
     return token
 
+
 def user_confirmed(user1):
-    #Получение id пользователя - удовлетворение условия "программа должна одинаково запускаться с ID и ника
+    # Получение id пользователя
     username_confirmation = False
     while not username_confirmation:
         response_id = requests.get(
@@ -49,18 +52,22 @@ def user_confirmed(user1):
         except KeyError:
             correct_input = False
             while not correct_input:
-                function_suspension = input('Некорректное имя пользователя. Хотите продолжить (Y/N):\n')
+                function_suspension = input('Некорректное имя пользователя. '
+                                            'Хотите продолжить (Y/N):\n')
                 if function_suspension.lower() == 'n':
-                    return 'Программа остановлена пользователем. Некорректное имя пользователя'
+                    return 'Программа остановлена пользователем. ' \
+                           'Некорректное имя пользователя'
                 elif function_suspension.lower() == 'y':
-                    user1 = input('Пожалуйста, введите имя пользователя или его id:\n')
+                    user1 = input('Пожалуйста, введите имя пользователя'
+                                  ' или его id:\n')
                     correct_input = True
                 else:
                     print('Некорректная команда. Повторите ввод')
     return user_id
 
+
 def user_friends(user_id, max_friends=1000):
-    #Получение списка друзей
+    # Получение списка друзей
     friend_count = 0
     friend_limit = 0
     response_friends_list = requests.get(
@@ -71,7 +78,7 @@ def user_friends(user_id, max_friends=1000):
             'v': 5.103
         }
     )
-    #получение ID друзей
+    # получение ID друзей
     pprint(response_friends_list.json())
     id_list = []
     ids = str(response_friends_list.json()['response']['items'][0])
@@ -89,8 +96,10 @@ def user_friends(user_id, max_friends=1000):
     id_list.append(ids)
     return id_list
 
+
 def solitary_group(user_id, id_list, maxpeople=0, grouplimit=1000):
-    #Получение списка групп целевого пользователя и составление списка словарей групп
+    # Получение списка групп целевого пользователя и
+    # составление списка словарей групп
     response_user_groups = requests.get(
         'https://api.vk.com/method/groups.get',
         params={
@@ -106,9 +115,12 @@ def solitary_group(user_id, id_list, maxpeople=0, grouplimit=1000):
     for user_group in response_user_groups.json()['response']['items']:
         group_count += 1
         if group_count > grouplimit:
-            print(f'Программа превысила лимит групп равный {grouplimit}, оставшиеся группы не будут проанализированы')
+            print(f'Программа превысила лимит групп '
+                  f'равный {grouplimit}, оставшиеся группы'
+                  f' не будут проанализированы')
             break
-        print(f'Проверено сообществ: {group_count}/{len(response_user_groups.json()["response"]["items"])}')
+        print(f'Проверено сообществ: {group_count}/'
+              f'{len(response_user_groups.json()["response"]["items"])}')
         time.sleep(1)
         friend_member = 0
         for ids in id_list:
@@ -122,21 +134,27 @@ def solitary_group(user_id, id_list, maxpeople=0, grouplimit=1000):
                     'v': 5.103
                 }
             )
-            #проверка наличия друзей в группе?
+            # проверка наличия друзей в группе?
             try:
-                for user_membership in response_mutual_groups.json()['response']:
+                for user_membership in\
+                        response_mutual_groups.json()['response']:
                     if user_membership['member'] == 1:
                         friend_member += 1
             except KeyError:
                 pass
         if friend_member <= maxpeople:
-            user_group_list.append({'name': user_group['name'], 'gid': user_group['id'], 'members:': user_group['members_count']})
+            user_group_list.append({'name': user_group['name'],
+                                    'gid': user_group['id'],
+                                    'members:': user_group['members_count']})
     return user_group_list
+
 
 APP_ID = 7423649
 TOKEN = token_confirmation(APP_ID)
-username = input('Пожалуйста, введите имя пользователя или его id:\n')
-spy_report = {'report': solitary_group(user_confirmed(username), user_friends(user_confirmed(username)))}
+username = input('Пожалуйста, введите имя пользователя или'
+                 ' его id:\n')
+spy_report = {'report': solitary_group(user_confirmed(username),
+                                       user_friends(user_confirmed(username)))}
 
 with open('groups.json', 'w') as report_file:
     json.dump(spy_report, report_file)
